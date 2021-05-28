264abdb259e2c30db0abbb12f73357be3b16cd50
nhithanh1998 <69423339+nhithanh1998@users.noreply.github.com>
2021-05-29 05:26:40 +0700

    update loading state in services

--- src/hooks/socket.hook.js
+++ src/hooks/socket.hook.js
@@ -1,9 +1,9 @@
 import io from 'socket.io-client'
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {useDispatch, useSelector} from 'react-redux'
+import {useDispatch} from 'react-redux'
 import {setGeneratedImageAccessURL} from '../redux/slicers/generated-image.slicer'
 import {setOriginImage} from '../redux/slicers/origin-image.slicer'
-
+import { setIsLoading } from '../redux/slicers/is-loading.slicer'
 
 export function useSocket() {
     const socket = io('ws://192.168.1.26:3000')
@@ -15,13 +15,15 @@ export function useSocket() {
     })
 
     socket.on('UPLOAD_IMAGE_SUCCESS', async data => {
-        await dispatch(setOriginImage(data))
+        dispatch(setOriginImage(data))
+        dispatch(setIsLoading(false))
     })
     
 
     socket.on('TRANSFER_COMPLETED', async data => {
         const {accessURL, styleID} = data
-        await dispatch(setGeneratedImageAccessURL({accessURL: accessURL, styleID}))
+        dispatch(setGeneratedImageAccessURL({accessURL: accessURL, styleID}))
+        dispatch(setIsLoading(false))
     })
     return socket
 }
