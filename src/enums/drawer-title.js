96da9347e4a7b77f38d5862fc7932267f3508b9a
nhithanh1998 <69423339+nhithanh1998@users.noreply.github.com>
2021-06-04 13:32:56 +0700

    update

---
+++ src/enums/drawer-title.js
@@ -0,0 +1,7 @@
+export const PROFILE_TITLE = "Profile"
+export const SIGN_OUT_TITLE = "Sign Out"
+export const HOME_TITLE = "Home"
+export const GALLERY_TITLE = "Gallery"
+export const ALBUM_TITLE = "Album"
+export const CAMERA_TITLE = "Camera"
+export const SECTION_TITLE = "Sections"
