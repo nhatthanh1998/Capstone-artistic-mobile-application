1cab1b83f60af3b15707875fcfe1b19728920caf
Marumi <30461021+nhatthanh1998@users.noreply.github.com>
2021-06-10 14:50:20 +0700

    update UI

--- src/components/AlbumPage/PhotoDetail.jsx
+++ src/components/AlbumPage/PhotoDetail.jsx
@@ -43,13 +43,14 @@ export const PhotoDetail = ({photo, visible, handlePressBack}) => {
         }
     }, [photo])
 
+
     if(photo === null) {
         return <Text>No photo selected!</Text>
     } else {
         return (
             <Modal style={tailwind("m-0")} isVisible={visible}>
-                <View style={tailwind("w-full h-full bg-gray-900")}>
-                    <View style={tailwind("flex flex-row z-20 absolute py-5 w-full")}>
+                <View style={tailwind("w-full h-full relative")}>
+                    <View style={{...tailwind("flex flex-row absolute py-5 w-full z-20 bg-gray-900")}}>
                         <TouchableOpacity style={tailwind("w-1/3 pl-5")} onPress={() => {handlePressBack()}}>
                             <Image source={require('../../assets/icons/left-arrow.png')} style={tailwind("h-5 w-5")}/>
                         </TouchableOpacity>
@@ -60,32 +61,39 @@ export const PhotoDetail = ({photo, visible, handlePressBack}) => {
                             <TouchableOpacity onPress={() => setShowMenu(!showMenu)}>
                                 <Image source={require('../../assets/icons/more.png')} style={tailwind("h-5 w-5")}/>
                             </TouchableOpacity>
-                            {showMenu ? <View style={{...tailwind("py-4 px-6 mt-4 rounded-xl"), ...styles.darken_2}} hide>
-                                <TouchableOpacity style={tailwind("flex flex-row w-full items-center py-2")}>
-                                    <Image style={tailwind("w-3 h-3 mr-6")} source={require('../../assets/icons/share.png')}></Image>
-                                    <Text style={tailwind("text-xs font-thin text-white")}>Share</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity style={tailwind("flex w-full flex-row items-center py-2")}
-                                onPress = {() => {
-                                    handleDownload()
-                                }}
-                                >
-                                    <Image style={tailwind("w-3 h-3 mr-6")} source={require('../../assets/icons/download.png')}></Image>
-                                    <Text style={tailwind("text-xs font-thin text-white")}>Download</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity style={tailwind("flex w-full flex-row items-center py-2")}
-                                onPress = {() => {handlePressDeleteButton()}}
-                                >
-                                    <Image style={tailwind("w-3 h-3 mr-6")} source={require('../../assets/icons/delete.png')}></Image>
-                                    <Text style={tailwind("text-xs font-thin text-white")}>Delete</Text>
-                                </TouchableOpacity>
-                            </View>: null}
                         </View>
                     </View>
-    
-                    <View style="z-10 relative">
+                    {
+                        showMenu ? (
+                        <View style={{...tailwind("py-4 z-20 absolute right-0 px-6 mt-14 mr-5 rounded-xl"), ...styles.darken_2}} hide>
+                            <TouchableOpacity style={tailwind("flex flex-row w-full items-center py-2")} onPress={() => console.log("Press")}>
+                                <Image style={tailwind("w-3 h-3 mr-6")} source={require('../../assets/icons/paint.png')}></Image>
+                                <Text style={tailwind("text-xs font-thin text-white")}>Transfer</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={tailwind("flex flex-row w-full items-center py-2")} onPress={() => console.log("Press")}>
+                                <Image style={tailwind("w-3 h-3 mr-6")} source={require('../../assets/icons/share.png')}></Image>
+                                <Text style={tailwind("text-xs font-thin text-white")}>Share</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={tailwind("flex w-full flex-row items-center py-2")}
+                            onPress = {() => {
+                                handleDownload()
+                            }}
+                            >
+                                <Image style={tailwind("w-3 h-3 mr-6")} source={require('../../assets/icons/download.png')}></Image>
+                                <Text style={tailwind("text-xs font-thin text-white")}>Download</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={tailwind("flex w-full flex-row items-center py-2")}
+                            onPress = {() => {handlePressDeleteButton()}}
+                            >
+                                <Image style={tailwind("w-3 h-3 mr-6")} source={require('../../assets/icons/delete.png')}></Image>
+                                <Text style={tailwind("text-xs font-thin text-white")}>Delete</Text>
+                            </TouchableOpacity>
+                        </View>
+                        ) : null
+                    }
+                    <View style={tailwind("pt-20 flex flex-row items-center justify-center w-full h-full bg-gray-900")}>
                         <Image
-                            style={{height: originImageHeight}} 
+                            style={{height: originImageHeight, ...tailwind("w-full")}} 
                             source={{uri: photo.accessURL}}
                             resizeMode="contain"/>  
                     </View>
