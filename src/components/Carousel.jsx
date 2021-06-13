6c1058868950b5a449fca9f19c5c085d9f4bf955
Marumi <30461021+nhatthanh1998@users.noreply.github.com>
2021-06-13 10:24:27 +0700

    updateUi

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -17,6 +17,37 @@ const ENTRIES1 = [
 ];
 const {width: screenWidth} = Dimensions.get('window');
 
+const animatedStyle = (index, animatedValue, carouselProps) => {
+  let animatedOpacity = {};
+  let animatedScale = {};
+
+  if (carouselProps.inactiveSlideOpacity < 1) {
+      animatedOpacity = {
+          opacity: animatedValue.interpolate({
+              inputRange: [0, 1],
+              outputRange: [carouselProps.inactiveSlideOpacity, 1]
+          })
+      };
+  }
+  if (carouselProps.inactiveSlideScale < 1) {
+      animatedScale = {
+          transform: [{
+              scaleY: animatedValue.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [carouselProps.inactiveSlideScale, 1]
+              })
+          }]
+      };
+  }
+  return {
+    ...animatedOpacity,
+    ...animatedScale
+  }
+}
+
+const activeStyle = tailwind("w-7 h-1.5 mr-1.5 bg-black rounded-full")
+const inactiveStyle = tailwind("w-1.5 h-1.5 mr-1.5 bg-gray-300 rounded-full")
+
 export const MyCarousel = props => {
   const [entries, setEntries] = useState([]);
   const [activeIndex, setActiveIndex] = useState(0)
@@ -33,21 +64,28 @@ export const MyCarousel = props => {
   const renderItem = ({item, index}) => {
     return (
         <View style={{...styles.shadow_4}}>
-            <Image style={tailwind("h-72 rounded-xl mx-1")} source={{uri: item.imgUrl}}/>
+            <Image style={tailwind("h-64 rounded-xl mx-2")} source={{uri: item.imgUrl}}/>
         </View>
     )
   };
 
   return (
-    <View>
-      <TouchableOpacity onPress={goForward}>
-        <Text>go to next slide</Text>
-      </TouchableOpacity>
+    <View style={tailwind("pt-5")}>
+      <View style={tailwind("absolute flex flex-row items-center top-0 right-0 mr-5")}>
+        <View style={activeIndex == 0 ? activeStyle : inactiveStyle}></View>
+        <View style={activeIndex == 1 ? activeStyle : inactiveStyle}></View>
+        <View style={activeIndex == 2 ? activeStyle : inactiveStyle}></View>
+
+      </View>
       <Carousel
-        ref={carouselRef}
+        onSnapToItem={(index) => {
+          setActiveIndex(index)
+        }}
+        loop
         enableSnap
+        slideInterpolatedStyle={animatedStyle}
+        ref={carouselRef}
         sliderWidth={screenWidth}
-        sliderHeight={screenWidth}
         itemWidth={screenWidth * 1/2}
         data={entries}
         renderItem={renderItem}
