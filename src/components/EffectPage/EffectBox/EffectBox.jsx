b9572c135fc23d4a54272255ee5e6c83abe5398d
Thanh.HoNhatTran@coxautoinc.com <Thanh.HoNhatTran@coxautoinc.com>
2021-08-10 15:10:27 +0700

    fix bug

--- src/components/EffectPage/EffectBox/EffectBox.jsx
+++ src/components/EffectPage/EffectBox/EffectBox.jsx
@@ -5,22 +5,23 @@ export const EffectBox = ({ style, handlePress, isSelect }) => {
     const {styleName, iconURL} = style
     if (isSelect == false) {
         return (
-            <View style={tailwind("w-28 h-28 py-5 flex flex-row")}>
+            <View style={tailwind("")}>
                 <TouchableOpacity
-                    style={tailwind("flex items-center w-full h-full")}
+                    style={tailwind("flex items-center w-full h-full px-5 py-3")}
                     onPress={() => { handlePress(style) }}
                 >
                     <View style={tailwind("w-20 h-20 rounded-lg overflow-hidden")}>
                         <Image source={{uri: iconURL}}
                             style={tailwind("w-full h-full")} />
                     </View >
-                    <Text style={tailwind("text-sm pt-2 text-center")}>{styleName}</Text>
+                    <Text style={tailwind("text-center text-xs text-gray-800 mt-1")}>{styleName}</Text>
                 </TouchableOpacity>
             </View>
+            
         )
     } else {
         return (
-            <View style={tailwind("w-28 h-28 py-5 flex flex-row")}>
+            <View style={tailwind("w-28 h-28 py-3 flex flex-row")}>
                 <TouchableOpacity
                     style={tailwind("flex items-center w-full h-full")}
                     onPress={() => { handlePress(style) }}
@@ -32,7 +33,7 @@ export const EffectBox = ({ style, handlePress, isSelect }) => {
                                 <Image source={require("../../../commons/images/checkicon.png")} style = {tailwind("w-10 h-10 absolute mt-4 ml-4")}/>
                             </ImageBackground>
                     </View >
-                    <Text style={tailwind("text-sm pt-2 text-center")}>{styleName}</Text>
+                    <Text style={tailwind("text-center text-xs mt-1 text-gray-800")}>{styleName}</Text>
                 </TouchableOpacity>
             </View>
         )
